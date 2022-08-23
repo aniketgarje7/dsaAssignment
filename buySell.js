9c75a8bcff68ce67a2c612840cabf7428583bf95
aniketgarje7 <108969449+aniketgarje7@users.noreply.github.com>
2022-08-23 16:45:43 +0530

    Update buySell.js

--- buySell.js
+++ buySell.js
@@ -1,11 +1,20 @@
 <script>
 
     function maxProfit(prices , size) {
-        var maxProfit = 0;
-        for (i = 1; i < size; i++)
-            if (prices[i] > prices[i - 1])
-                maxProfit += prices[i] - prices[i - 1];
-        return maxProfit;
+       let left = 0; // Buy
+  let right = 1; // sell
+  let max_profit = 0;
+  while (right < prices.length) {
+    if (prices[left] < prices[right]) {
+      let profit = prices[right] - prices[left]; 
+
+      max_profit = Math.max(max_profit, profit);
+    } else {
+      left = right;
+    }
+    right++;
+  }
+  return max_profit;
     }
   
     
