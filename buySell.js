39df412831ebf9caf9139aa0ba375538551ff805
aniketgarje7 <108969449+aniketgarje7@users.noreply.github.com>
2022-08-23 16:23:38 +0530

    Create buySell.js

---
+++ buySell.js
@@ -0,0 +1,17 @@
+<script>
+
+    function maxProfit(prices , size) {
+        var maxProfit = 0;
+        for (i = 1; i < size; i++)
+            if (prices[i] > prices[i - 1])
+                maxProfit += prices[i] - prices[i - 1];
+        return maxProfit;
+    }
+  
+    
+        var price = [ 100, 180, 260, 310, 40, 535, 695 ];
+        var n = price.length;
+        document.write(maxProfit(price, n));
+  
+
+</script>
